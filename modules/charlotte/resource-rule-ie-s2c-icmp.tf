9c40fff01d1feef1c8a77e4dcc7888a87040fb5c
putilin.dmitriy3 <putilin.dmitriy3@wb.ru>
2024-03-08 06:08:39 +0000

    Update file resource-rule-ie-s2c-icmp.tf

--- modules/charlotte/resource-rule-ie-s2c-icmp.tf
+++ modules/charlotte/resource-rule-ie-s2c-icmp.tf
@@ -1,1 +1,29 @@
-#TODO
+# Terraform V2
+resource "sgroups_ie_icmp_rules" "rules" {
+  depends_on = [
+    sgroups_groups.groups,
+  ]
+  items = {
+    for key, value in local.rules_map_all_ie_s2c_by_proto_map :
+      "icmp${split("icmpIPv", value.transport).1}:cidr(${value.cidr})sg(${value.sg_name})${value.traffic}" => {
+
+        traffic     = value.traffic
+
+        sg_name     = value.sg_name
+        cidr        = value.cidr
+
+        ip_v    = split("icmp", value.transport).1
+        logs    = value.logs
+        trace   = value.trace
+
+        type = flatten([
+          for item in value.access: [item.type]
+        ])
+      }
+
+    if contains(["icmpIPv6:ingress",
+                 "icmpIPv4:ingress",
+                 "icmpIPv6:egress",
+                 "icmpIPv4:egress"], "${value.transport}:${value.traffic}")
+  }
+}
